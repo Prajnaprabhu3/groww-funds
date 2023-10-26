906e1eacd19ae88d80c456dbff29b3c135d52888
Prajna <prajnapprabhu3@gmail.com>
2023-10-26 23:57:26 +0530

    ref: folder structure

---
+++ src/providers/ThemeProvider.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+
+export default function ThemeProvider({ children }) {
+  return <NextThemeProvider>{children}</NextThemeProvider>;
+}
